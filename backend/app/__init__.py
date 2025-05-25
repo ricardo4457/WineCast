1d57f66daf1886fc659708670e0bb85c7299f429
ivocucas <ivo.oliveira.2022267@my.istec.pt>
2025-05-26 00:27:20 +0100

    refactor:remove comment

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -17,9 +17,5 @@ def create_app():
     # Create all Tables
     with app.app_context():
         db.create_all()
-    #teste
-    # Register error handlers, if any
-    # app.register_error_handler(404, not_found_error)  
-    # app.register_error_handler(500, internal_server_error)
-    # Register other blueprints or extensions as needed
+
     return app
