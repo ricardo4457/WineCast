2708b65e66b872ebde6cf5060d9e53894ba9949e
Ricardo Vieira <57718264+ricardo4457@users.noreply.github.com>
2025-06-16 00:17:08 +0100

    addionar alterações em falta

--- frontend-winecast/src/main.jsx
+++ frontend-winecast/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./AppRoutes.jsx";
+import App from "./App.jsx";
 // Ferramentas do Redux
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
